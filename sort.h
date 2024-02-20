dd56117ac8484f719c7a66b9a4f303c2d51ed4d7
vintage254 <derricknjuguna414@gmail.com>
2024-02-19 23:17:56 -0800

    shell_sort

--- sort.h
+++ sort.h
@@ -35,18 +35,19 @@ typedef enum bool
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-/*task 2*/
+/*task 0*/
 void bubble_sort(int *array, size_t size);
-void swap_ints(int *a, int *b);
+void swap_function(int *a, int *b);
 /*task 1*/
 void swap_nodes(listint_t **header, listint_t **node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
-/*task 3*/
+/*task 2*/
 void selection_sort(int *array, size_t size);
-/*task 4*/
+/*task 3*/
 void quick_sort(int *array, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
-
+/*task 4*/
+void shell_sort(int *array, size_t size);
 #endif/*SORT_H*/
