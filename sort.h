38347cbc23ab6118ccaee655b363ba22713b0a52
sambutracy <tracysambut@gmail.com>
2024-02-20 02:33:04 -0800

    advanced task 101

--- sort.h
+++ sort.h
@@ -50,4 +50,7 @@ int partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 /*task 4*/
 void shell_sort(int *array, size_t size);
+/*task 5*/
+void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2);
 #endif/*SORT_H*/
